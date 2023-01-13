5d73394b85eecd694dcb5f53e4301f54a9525f11
nadya <Kowalewskaya91@ya.ru>
2023-01-13 16:16:18 +0300

    Refactoring step 5

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,14 +7,16 @@ const rulesOfTheGame = 'Find the greatest common divisor of given numbers.';
 
 const findDivider = (lesserNumber, num1, num2) => {
   let divider = 0;
+
   for (let i = 2; i <= lesserNumber; i += 1) {
     if (num1 % i === 0 && num2 % i === 0) {
       divider = i;
     }
+    if (divider === 0) {
+      divider = 1;
+    }
   }
-  if (divider === 0) {
-    divider = 1;
-  }
+
   return divider;
 };
 
@@ -23,7 +25,7 @@ const genQuestionAndAnswer = () => {
   const num2 = genRandomNumber(1, 100);
   const arr = [num1, num2];
   const lesserNumber = Math.min(...arr);
-  const divider = findDivider(lesserNumber);
+  const divider = findDivider(lesserNumber, num1, num2);
   const randomNumbers = arr.join(' ');
   return { question: `${randomNumbers}`, rightAnswer: String(divider) };
 };
